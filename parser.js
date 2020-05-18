87659be3b01c26c2f964344a2e060f78f284f1a7
Josh Helmink <josh.helmink@gmail.com>
2020-05-18 17:47:03 +1200

    Move datetime string extensions and functions
    Moved to separate file to declutter main parser code

--- parser.js
+++ parser.js
@@ -4,88 +4,6 @@ var config = {};
 var myEmailAddress = null;
 var myDisplayName = null;
 
-String.prototype.limit = function (limit) {
-    return this.length > limit ? this.substr(0, limit) + '...' : this;
-}
-
-String.prototype.toHHMMSS = function () {
-    // don't forget the second param
-    var secNum = parseInt(this, 10);
-    var hours = Math.floor(secNum / 3600);
-    var minutes = Math.floor((secNum - (hours * 3600)) / 60);
-    var seconds = secNum - (hours * 3600) - (minutes * 60);
-
-    if (hours < 10) {
-        hours = '0' + hours;
-    }
-    if (minutes < 10) {
-        minutes = '0' + minutes;
-    }
-    if (seconds < 10) {
-        seconds = '0' + seconds;
-    }
-    var time = hours + 'h ' + minutes + 'm ' + seconds + 's';
-    return time;
-}
-
-String.prototype.toHHMM = function () {
-    // don't forget the second param
-    var secNum = parseInt(this, 10);
-    var hours = Math.floor(secNum / 3600);
-    var minutes = Math.floor((secNum - (hours * 3600)) / 60);
-
-    // set minimum as 1 minute
-    if (hours + minutes === 0) minutes = 1;
-
-    // pad zero
-    if (hours < 10) {
-        hours = '0' + hours;
-    }
-    // pad zero
-    if (minutes < 10) {
-        minutes = '0' + minutes;
-    }
-
-    var time = hours + 'h ' + minutes + 'm';
-    return time;
-}
-String.prototype.toHH_MM = function () {
-    // don't forget the second param
-    var secNum = parseInt(this, 10);
-    var hours = Math.floor(secNum / 3600);
-    var minutes = Math.floor((secNum - (hours * 3600)) / 60);
-
-    if (hours < 10) {
-        hours = '0' + hours;
-    }
-    if (minutes < 10) {
-        minutes = '0' + minutes;
-    }
-
-    var time = hours + ':' + minutes;
-    return time;
-}
-String.prototype.toDDMM = function () {
-    // don't forget the second param
-    var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    var d = new Date(this);
-    return monthNames[d.getMonth()] + ' ' + d.getDate();
-    // return d.getDate() + '.' + (d.getMonth() + 1) + '.';
-}
-
-function createDateKey(date) {
-    var concatZero = (value) => {
-        if (value < 10) {
-            return '0' + value;
-        } else {
-            return '' + value;
-        }
-    }
-
-    var d = new Date(date);
-    return '' + d.getFullYear() + concatZero(d.getMonth() + 1) + concatZero(d.getDate());
-}
-
 $(document).ready(function () {
 
     chrome.storage.sync.get({
@@ -128,8 +46,6 @@ $(document).ready(function () {
         fetchEntries();
     });
 });
-
-
 
 function getMyData() {
     $.get(config.url + '/rest/api/2/myself',
@@ -207,10 +123,10 @@ function fetchEntries() {
         entries.forEach(function (entry) {
             entry.description = entry.description || 'no-description';
             var issue = entry.description.split(' ')[0];
-            var togglTime = roundUpTogglDuration(entry.duration, config.roundMinutes);
-
-            var dateString = toJiraWhateverDateTime(entry.start);
-            var dateKey = createDateKey(entry.start);
+            
+            var togglTime = dateTimeHelpers.roundUpTogglDuration(entry.duration, config.roundMinutes);
+            var dateString = dateTimeHelpers.toJiraWhateverDateTime(entry.start);
+            var dateKey = dateTimeHelpers.createDateKey(entry.start);
 
             var log = _.find(logs, function (log) {
                 if (config.mergeEntriesBy === 'issue-and-date') {
@@ -243,60 +159,6 @@ function fetchEntries() {
 
         renderList();
     });
-}
-
-/*
-* Round duration up to next `minutes`.
-* No rounding will be applied if minutes is zero.
-* 
-* Example: round to next quater:
-* roundUpTogglDuration(22, 15) = 30 // rounded to the next quarter
-* roundUpTogglDuration(35, 60) = 60 // round to full hour
-* roundUpTogglDuration(11, 0) = 11  // ignored rounding
-*/
-function roundUpTogglDuration(initialDurationSeconds, roundingMinutes) {
-    var minutesDuration = initialDurationSeconds / 60 // initialDuration is in seconds
-    if (minutesDuration == 0 || roundingMinutes == 0) { // no rounding required
-        return initialDurationSeconds;
-    } else { // make sure minium `minutes` are tracked
-        var roundedDuration = (Math.floor(minutesDuration / roundingMinutes) + 1) * roundingMinutes;
-        return roundedDuration * 60; // convert back to seconds
-    }
-}
-
-function toJiraWhateverDateTime(date) {
-    // TOGGL:           at: "2016-03-14T11:02:55+00:00"
-    // JIRA:     "started": "2012-02-15T17:34:37.937-0600"
-
-    // toggl time should look like jira time (otherwise 500 Server Error is raised)
-
-    var parsedDate = Date.parse(date);
-    var jiraDate = Date.now();
-
-    if (parsedDate) {
-        jiraDate = new Date(parsedDate);
-    }
-
-    var dateString = jiraDate.toISOString();
-
-    // timezone is something fucked up with minus and in minutes
-    // thatswhy divide it by -60 to get a positive value in numbers
-    // example -60 -> +1 (to convert it to GMT+0100)
-    var timeZone = jiraDate.getTimezoneOffset() / (-60);
-    var absTimeZone = Math.abs(timeZone);
-    var timeZoneString;
-    var sign = timeZone > 0 ? '+' : '-';
-
-    // take absolute because it can also be minus
-    if (absTimeZone < 10) {
-        timeZoneString = sign + '0' + absTimeZone + '00'
-    } else {
-        timeZoneString = sign + absTimeZone + '00'
-    }
-
-    dateString = dateString.replace('Z', timeZoneString);
-
-    return dateString;
 }
 
 function renderList() {
